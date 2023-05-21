290551f759ad86ef19e4c2a9f093381babc06483
hassan <hassan@yahoo.com>
2023-05-21 09:35:34 +0330

    Singinform

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header-footer/header";
 import Footer from "./Components/Header-footer/footer";
 import Home from "./Components/Home/index";
+import SingIn from "./Components/Singin";
 
 const Root = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/sing_in" element={<SingIn />} />
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
